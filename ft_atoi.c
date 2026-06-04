2bcf670d38ec5f8d3a8bf91183f6ae0d56180d7e
Jo <jordanmoret4@gmail.com>
2026-06-04 10:21:17 +0200

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,33 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jordanmoret <jordanmoret@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/26 16:46:54 by jordanmoret       #+#    #+#             */
-/*   Updated: 2026/06/04 10:17:45 by jordanmoret      ###   ########.fr       */
+/*   Created: 2026/06/04 09:51:28 by jordanmoret       #+#    #+#             */
+/*   Updated: 2026/06/04 10:17:58 by jordanmoret      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-
-typedef struct s_list 
+int		ft_atoi(const char *str)
 {
-    void            *content;
-    struct s_list   *next;
-}   t_list;
-
-
-void    ft_bzero(void *s, size_t n);
-int     ft_isdigit(int c);
-
-
-
-
-#endif
+	int i;
+	int n;
+	int sign;
+	
+	i = 0;
+	n = 0;
+	sign = 1;
+	while((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	 {
+		n = n * 10 + (str[i] - 48);
+		i++;
+	 }
+	 return (n * sign)
+};
